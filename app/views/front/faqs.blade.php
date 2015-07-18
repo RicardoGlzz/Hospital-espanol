e95144de3cd3758c2f64e5e834390bfa0aca06c5
Ricardo Gonzalez <ricardogonzalezlopez1@gmail.com>
2015-07-18 16:23:01 -0500

    faqs

--- app/views/front/faqs.blade.php
+++ app/views/front/faqs.blade.php
@@ -3,18 +3,70 @@
 <section class="wrapper-section">
 	<section id="faqs">
 		<h2>This is a list of common questions, if you have any other questions, be sure to contact us in this <a href="{{ URL::to('contact') }}">link</a></h2>
-		<h3>lorem impsum late loda dkode absil dore mi?</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, assumenda, architecto eligendi voluptatem aspernatur voluptas nesciunt vero optio modi cumque necessitatibus. Esse harum culpa tenetur ratione voluptate possimus, porro aliquid.</p>
-		<h3>lorem impsum late loda dkode absil dore mi?</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, assumenda, architecto eligendi voluptatem aspernatur voluptas nesciunt vero optio modi cumque necessitatibus. Esse harum culpa tenetur ratione voluptate possimus, porro aliquid.</p>
-		<h3>lorem impsum late loda dkode absil dore mi?</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, assumenda, architecto eligendi voluptatem aspernatur voluptas nesciunt vero optio modi cumque necessitatibus. Esse harum culpa tenetur ratione voluptate possimus, porro aliquid.</p>
-		<h3>lorem impsum late loda dkode absil dore mi?</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, assumenda, architecto eligendi voluptatem aspernatur voluptas nesciunt vero optio modi cumque necessitatibus. Esse harum culpa tenetur ratione voluptate possimus, porro aliquid.</p>
-		<h3>lorem impsum late loda dkode absil dore mi?</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, assumenda, architecto eligendi voluptatem aspernatur voluptas nesciunt vero optio modi cumque necessitatibus. Esse harum culpa tenetur ratione voluptate possimus, porro aliquid.</p>
-		<h3>lorem impsum late loda dkode absil dore mi?</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, assumenda, architecto eligendi voluptatem aspernatur voluptas nesciunt vero optio modi cumque necessitatibus. Esse harum culpa tenetur ratione voluptate possimus, porro aliquid.</p>
+		<h3>What can you expect after bariatric surgery?</h3>
+		<p>You can expect to lose between 65% and 100% of your excess weight in 12 months.
+		This weight loss helps improve pre-existing conditions and prevent diseases.</p>
+		<br>
+		<h3>When I can start exercising again after surgery?</h3>
+		<p>You can usually begin within 14 days.
+		Take care of your joints (knees and ankles) to exercising, if you have any problems or do not want to put too much pressure on your joints, we recommend stationary bicycle, water aerobics or walking under water (remember to wait 10 days after surgery to immerse in water).</p>
+		<br>
+		<h3>Would it improve my type 2 diabetes?</h3>
+		<p>Yes. Studies have shown that over 95% of bariatric patients have improvement of type 2 diabetes and 85% has a total elimination.
+		The metabolic process is excellent choice for people suffering from type 2 diabetes.</p>
+		<br>
+		<h3>Can I get pregnant after a bariatric procedure?</h3>
+		<p>Si. Después de la Manga y la Banda Gástrica te puedes embarazar de inmediato pero se recomienda esperar a que llegues a tu peso ideal.
+		Después del Bypass Gástrico es necesario esperar 18 meses debido a la deficiencia en nutrientes durante la pérdida de peso.
+		Estudios han comparado embarazos de pacientes bariátricos con las de personas afectadas por la obesidad; los pacientes bariátricos tienen un índice menor de preeclampsia, diabetes gestacional y abortos involuntarios.
+		Los hijos de los pacientes bariátricos también son menos propensos a bajo peso o sobrepeso al nacer.</p>
+		<br>
+		<h3>Will I lose my hair after bariatric surgery?</h3>
+		<p>Some patients temporarily lose some hair between the sixth and ninth month after the bypass procedure.
+		Your postoperative visits with lic. Paulina Rámirez specialist bariatric nutrition, adjustments are made on your intake of protein, vitamins and minerals to prevent falls.
+		It is important to know that hair loss from nutritional causes is only temporary and reversible.</p>
+		<br>
+
+		<h3>Is there a diet before the procedure?</h3>
+		<p>Yes. This diet is made in order to reduce the size of the liver and abdominal fat to facilitate the procedure and recovery.</p>
+		<br>
+		<h3>Can I remove my medications after the procedure?</h3>
+		<p>With weight loss it is likely to stop taking medications related to obesity such as hypertension and diabetes.
+		It is important to monitor your development going to be doing the appropriate changes.</p>
+		<br>
+		<h3>Can I reach my target weight with diet and exercise alone?</h3>
+		<p>Yes, but it is difficult for people with severe obesity are successful with diet and exercise. When diet extends the body makes changes reducing biological metabolism, increasing the caloric intake and hunger. When it "breaks" occur other diet changes that help increase obesity.
+		Slower metabolism, increased fat accumulation and deterioration of the fat burning process predisposition are some of these changes.
+		The Panel of Experts of the National Institute of Health (USA) recognize that the loss of "long-term" weight for people with severe obesity is only possible through bariatric surgery.
+		According to scientific studies only 5% of people who are dieting are able to maintain weight loss long term (3-5 years).
+		Compared to 5% of the diets, 90% of bariatric patients have lost more than 50% overweight in the long term.</p>
+		<br>
+		<h3>When I can return to work after the procedure?</h3>
+		<p>If your work is desktop you can return in three days with gastric banding, sleeve and six days to seven days with gastric bypass. For metabolic surgery it depends on the individual case of the patient.
+		If your work is physically demanding a longer period than 15 days is necessary.</p>
+		<br>
+		<h3>Will I have excess skin after weight loss?</h3>
+		<p>This depends on your genetics, excess weight, age, smoking status, physical inactivity, sun exposure, etc.
+		It is recommended to drink plenty of water after the procedure to maintain skin elasticity, like getting enough protein and exercise weightlifting to reaffirm the tissue.
+		There are patients who benefit from plastic surgery where excess skin is removed. That is recommended at least 18 months after your bariatric procedure.</p>
+		<br>	
+		<h3>Will I need to take vitamins and minerals?</h3>
+		<p>The vitamin and mineral supplements are recommended after any bariatric procedure, but are only needed after gastric bypass.</p>
+		<br>
+		<h3>Will I have to diet and exercise after the procedure?</h3>
+		<p>Yes. The bariatric procedure is a tool that helps you reach your target weight and maintain it.
+		It is an opportunity to change your eating and exercise habits to be at a healthy weight the rest of your life.</p>
+		<br>
+		<h3>What are the risks?</h3>
+		<p>Like any surgery bariatric surgery can present risks, some serious and others just annoying.
+		The risks depend on the health condition of the patient, the procedure to be performed and the surgeon's experience.
+		It is important to discuss these risks with your surgeon directly.</p>
+		<br>
+		<h3>Can I have health problems caused by deficiencies of vitamins and minerals?</h3>
+		<p>Bariatric procedure can cause deficiency in vitamins, minerals and proteins.
+		These can cause fatigue, anemia, loss of bone and muscle, impaired night vision, low immunity, loss of proper nerve function and even cognitive deficits.
+		Fortunately everything can be avoided with proper diet and use of dietary supplements.
+		Our patients receive nutritional recommendations before and after surgery where levels are monitored by our expert Dr. bariatric nutrition.</p>
 	</section>
 	
 </section>
